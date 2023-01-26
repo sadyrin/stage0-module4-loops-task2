54fac22eac30200790a63ae8feb6591fdeae5819
sadyrin <sadyrin@gmail.com>
2023-01-26 13:02:56 +0200

    Done task stage0-module4-loops-task2

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
-
+        int count = 1;
+        int lastResult = 1;
+        System.out.println(lastResult);
+        while (count <= printToInclusive){
+            lastResult *= count;
+            System.out.println(lastResult);
+            count++;
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,24 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        int primeCheckNumber;
+        int n = 1;
+        int divisibleCount;
+      
+        while (n <= printToInclusive) {
+         divisibleCount = 0;
+         primeCheckNumber = 2;
+         while (primeCheckNumber <= n / 2) {
+          if (n % primeCheckNumber == 0) {
+           divisibleCount++;
+           break;
+          }
+          primeCheckNumber++;
+         }
+         if (divisibleCount == 0 && n != 1) {
+          System.out.println(n);
+         }
+         n++;
+        }
     }
 }
